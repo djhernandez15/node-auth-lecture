b6d1e3068eebe5b590b63723cc19be607e4e236a
David Hernandez <djhernandez@Davids-MacBook-Air.local>
2019-04-18 14:14:35 -0500

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const session = require("express-session");
 const massive = require("massive");
+const bcrypt = require("bcryptjs");
 
 massive(process.env.CONNECTION_STRING).then(db => {
   app.set("db", db);
@@ -11,18 +13,47 @@ massive(process.env.CONNECTION_STRING).then(db => {
   }
 });
 
+app.use(
+  session({ secret: "househunters", resave: false, saveUninitialized: true })
+);
+
 app.use(express.json());
 
-app.post("/auth/login", (req, res) => {
-  // Login
+app.get("/auth/me", (req, res) => {
+  res.json(req.session.user);
 });
 
-app.post("/auth/signup", (req, res) => {
-  // Signup
+app.post("/auth/login", async (req, res) => {
+  const { username, password } = req.body;
+  const result = await req.app.get("db").getUser(username);
+  if (result.length > 0) {
+    //we need to check password
+    const isMatch = await bcrypt.compare(password, result[0].password);
+    if (isMatch) {
+      req.session.user = result[0].username;
+      res.json(username);
+    } else {
+      res.status(403).json("Incorrect username or password");
+    }
+  } else {
+    res.status(403).json("Incorrect username or password");
+  }
+});
+
+app.post("/auth/signup", async (req, res) => {
+  const { username, password } = req.body;
+  const hash = await bcrypt.hash(password, 10).catch(err => console.log(err));
+  const dbResult = await req.app
+    .get("db")
+    .addUser([username, hash])
+    .catch(err => console.log(err));
+  req.session.user = username;
+  res.json(dbResult);
 });
 
 app.post("/auth/logout", (req, res) => {
-  // Logout
+  req.session.destroy();
+  res.json("Successfully logged out!");
 });
 
 app.listen(5050, () => console.log("Listening on 5050"));
